7a663055a6fbecdc5feb506004422da492291aaf
KACPER GRZENDA - STUDENT <G00373427@gmit.ie>
2020-02-17 17:40:24 +0000

    lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+
+  counterPress:number = 0;
+  starText: string;
+  presidents: string[] = ["Higgins",
+    "McAleese",
+    "Robinson",
+    "Hillery"];
+
+  presidents2: object[] = 
+  [
+    {name:"Higgins", term:"2011-"},
+    {name:"McAleese", term:"1997-2011"},
+    {name:"Robinson", term:"1990-1997"},
+    {name:"Hillery", term:"1974-1997"}
+    ];
+    
+
+  countPress(){
+    this.counterPress++;
+  }
+
+  addText(){
+    if(this.starText == null)
+  this.starText = "Look at the Star";
+  else if (this.starText == "Look at the Star") {
+    this.starText = null;
+  }
+  }
 }
